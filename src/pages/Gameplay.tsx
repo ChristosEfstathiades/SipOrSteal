6170ad7e0e283edaf599015993a06b9884c21e51
Christos Efstathiades <46204113+ChristosEfstathiades@users.noreply.github.com>
2026-05-18 13:48:26 +0100

    Add gameplay buttons and suggested questions section

--- src/pages/Gameplay.tsx
+++ src/pages/Gameplay.tsx
@@ -1,7 +1,19 @@
+import { GameContext, SCREENS } from "../Contexts";
+import { useContext } from "react";
+
 export default function Gameplay() {
+    const {} = useContext(GameContext);
+
     return (
         <>
-            <div></div>
+
+            <div className="flex flex-row justify-around">
+                <button className="text-white text-2xl cursor-pointer font-medium capitalize bg-red-700 border-2 border-black/15 py-4 px-12 rounded-xl">Steal</button>
+                <button className="text-white text-2xl cursor-pointer font-medium capitalize bg-green-700 border-2 border-black/15 py-4 px-12 rounded-xl">Leave</button>
+            </div>
+            <div>
+                <h2>Suggested Questions:</h2>
+            </div>
         </>
     );
 }
